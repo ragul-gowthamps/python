f995d6f2937fa6cc71ae65b33c17e0f3f8a53986
ragul-gowthamps <ragul.gowthamps@gmail.com>
2025-04-26 01:11:21 +0100

    3rd-commit

---
+++ function_hello.py
@@ -0,0 +1,8 @@
+def hello():
+    print("Hello, " + name + "!")
+# This is a simple function that takes a name as input and prints a greeting message.   
+
+name = input("Enter your name: ")
+hello()
+
+print(name)
