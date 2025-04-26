e724f4484157dbfbb3834d38544ce6ac404ed6ac
ragul-gowthamps <ragul.gowthamps@gmail.com>
2025-04-26 23:30:00 +0100

    hello_function

--- function_hello.py
+++ function_hello.py
@@ -1,8 +1,13 @@
 def hello():
     print("Hello, " + name + "!")
+    
 # This is a simple function that takes a name as input and prints a greeting message.   
+def hello_to(to):
+    print("Hello, " + to + "!")
 
 name = input("Enter your name: ")
 hello()
 
 print(name)
+
+hello_to(name)
