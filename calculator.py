232407e6e9d01027421b84584290a11868e5e5b7
ragul-gowthamps <ragul.gowthamps@gmail.com>
2025-04-26 23:50:22 +0100

    update_on_calculator.py

--- calculator.py
+++ calculator.py
@@ -11,3 +11,20 @@ z1=round(x/y,2)
 # print(f"{z1:}") 
 # print(f"{z1:.2f}") # output with 2 decimal places
 print(f"{z1:,.2f}") # output with 2 decimal places and commas every third digit
+
+def main():
+    x =int(input("whats x? "))
+    print("x squared is", square(x))
+    print("x squared is", square_1(x))
+    print("x squared is", square_2(x))
+    
+def square(n):
+    return n * n
+
+def square_1(n):
+    return n ** 2
+
+def square_2(n):
+    return pow(n, 2)
+
+main()
