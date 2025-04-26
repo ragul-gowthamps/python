2d65952c5f25c1390daae6199aaf9da3b6f4aada
ragul-gowthamps <ragul.gowthamps@gmail.com>
2025-04-26 23:44:04 +0100

    hello_function_update

--- function_hello.py
+++ function_hello.py
@@ -1,13 +1,36 @@
-def hello():
-    print("Hello, " + name + "!")
-    
 # This is a simple function that takes a name as input and prints a greeting message.   
-def hello_to(to):
-    print("Hello, " + to + "!")
 
 name = input("Enter your name: ")
-hello()
 
 print(name)
 
+
+def hello():
+    print("Hello, " + name + "!")
+
+hello()
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+def hello_to(to):
+    print("Hello, " + to + "!")
+
 hello_to(name)
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+def hello_to_world(to="world"):
+    print("Hello, " + to + "!")
+    
+hello_to_world()
+hello_to_world(name)
+
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+
+def main():
+    new_name = input("Enter your new_name: ")
+    new_hello(new_name)
+      
+main()  
+def new_hello(new_parameter):
+    print("Hello, " + new_parameter + "!")
+
+#xxxxxxxxxxxxxxxxxxxxxxxxxxxx
