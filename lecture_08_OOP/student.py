498fb36f97ed307be600b899e62bc3af864def04
ragul-gowthamps <100247376+ragul-gowthamps@users.noreply.github.com>
2025-07-18 19:51:29 +0100

    first_py_in_oop update

--- lecture_08_OOP/student.py
+++ lecture_08_OOP/student.py
@@ -1,3 +1,59 @@
-name = input("Name:")
-house = input("House:")
-print(f"{name} from {house}")
+# name = input("Name:")
+# house = input("House:")
+# print(f"{name} from {house}")
+
+# //////////////////////////////////////
+# def main():
+#     student = get_student()
+#     if student[0] =="padma":
+#         student[1] = "Ravenclaw"
+#     print(f"{student[0]} from {student[1]}")
+    
+# # def get_name():
+# #     return input("Name: ")
+
+# # def get_house():
+# #     return input("House: ")
+
+# def get_student():
+#     name = input("Name: ")
+#     house = input("House: ")
+#     return [name, house]
+
+# if __name__ == "__main__":
+#     main()
+    
+    # ///////////////////
+
+# def main():
+#     student = get_student()
+#     print(f"{student['name']} from {student['house']}")
+
+# def get_student():
+#     student={}
+#     student["name"] = input("Name: ")
+#     student["house"] = input("House: ")
+#     return student
+
+# if __name__ == "__main__":
+#     main()
+      
+    # ///////////////////
+    
+    
+    
+
+def main():
+    student = get_student()
+    if student["name"].lower() == "padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['house']}")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
+
+if __name__ == "__main__":
+    main()
+      
