3d8862d493890406df02143887113bab4e064292
ragul-gowthamps <100247376+ragul-gowthamps@users.noreply.github.com>
2025-07-19 08:58:57 +0100

    class and methods

--- lecture_08_OOP/student.py
+++ lecture_08_OOP/student.py
@@ -43,17 +43,66 @@
     
     
 
+# def main():
+#     student = get_student()
+#     if student["name"].lower() == "padma":
+#         student["house"] = "Ravenclaw"
+#     print(f"{student['name']} from {student['house']}")
+
+# def get_student():
+#     name = input("Name: ")
+#     house = input("House: ")
+#     return {"name": name, "house": house}
+
+# if __name__ == "__main__":
+#     main()
+
+# ///////////////////      
+
+# class Student:
+#     ...
+    
+    
+    
+# def main():
+#     student = get_student()
+#     print(f"{student.name} from {student.house}")
+
+# def get_student():
+#     student = Student()
+#     student.name = input("Name: ")
+#     student.house = input("House: ")
+#     return student
+
+# if __name__ == "__main__":
+#     main()
+
+#//////////////////////////////////////////////////
+
+class Student:
+    def __init__(self,name, house):
+        if not name:
+            raise ValueError("Name cannot be empty")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
 def main():
     student = get_student()
-    if student["name"].lower() == "padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    print(student)
+    # print(f"{student.name} from {student.house}")
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return {"name": name, "house": house}
+    student = Student(name, house)
+    return student
+
+
 
 if __name__ == "__main__":
     main()
-      
