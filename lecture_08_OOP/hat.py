8e7c89fddbaf033a61c2a67f07b166e754a4f84a
ragul-gowthamps <100247376+ragul-gowthamps@users.noreply.github.com>
2025-07-19 10:26:16 +0100

    types of class

---
+++ lecture_08_OOP/hat.py
@@ -0,0 +1,8 @@
+class Hat:
+    def sort(self, name):
+        print(name, "is in" , ""somehousr")  
+    
+
+
+hat = Hat()
+hat.sort("Harry")
---
+++ lecture_08_OOP/type.py
@@ -0,0 +1,3 @@
+print(type(list()))
+print(type({}))
+print(type(dict()))
