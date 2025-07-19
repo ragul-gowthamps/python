f61057ca8cc95bd323158d511608996625ac5c89
ragul-gowthamps <100247376+ragul-gowthamps@users.noreply.github.com>
2025-07-19 10:20:15 +0100

    getter and setter

--- lecture_08_OOP/student.py
+++ lecture_08_OOP/student.py
@@ -78,29 +78,99 @@
 #     main()
 
 #//////////////////////////////////////////////////
+# #adding patronus and charm method
+# class Student:
+#     def __init__(self,name, house, patronus):
+#         if not name:
+#             raise ValueError("Name cannot be empty")
+#         house_lower = house.lower()
+#         if house_lower not in ["gryffindor", "hufflepuff", "ravenclaw", "slytherin"]:
+#             raise ValueError("Invalid house")
+#         self.name = name
+#         self.house = house
+#         self.patronus = patronus
+    
+#     def __str__(self):
+#         return f"{self.name} from {self.house}"
+    
+#     def charm(self):
+#         match self.patronus:
+#             case "stag":
+#                 return "Expecto Patronum stag!"
+#             case "otter":
+#                 return "Expecto Patronum otter!"
+#             case "doe":
+#                 return "Expecto Patronum doe!"
+#             case _:
+#                 return "Expecto Patronum unknown!"
+
+# def main():
+#     student = get_student()
+#     print(student)
+#     print(student.charm())  
+#     # print(f"{student.name} from {student.house}")
+
+# def get_student():
+#     name = input("Name: ")
+#     house = input("House: ")
+#     patronus = input("Patronus: ")
+#     # student = Student(name, house, patronus)
+#     return Student(name, house, patronus)
+
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+# ///////////////////////////////////////////////////////
 
 class Student:
     def __init__(self,name, house):
-        if not name:
-            raise ValueError("Name cannot be empty")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
         self.name = name
         self.house = house
-    
+            
     def __str__(self):
         return f"{self.name} from {self.house}"
 
+
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Name cannot be empty")
+        self._name = name   
+    #getter
+    @property
+    def house(self):
+        return self._house
+
+    #setter
+    @house.setter
+    def house(self, house):
+        house_lower = house.lower()
+        if house_lower not in ["gryffindor", "hufflepuff", "ravenclaw", "slytherin","stark"]:
+            raise ValueError("Invalid house")
+        self._house = house
+        
+        
 def main():
     student = get_student()
+    
     print(student)
-    # print(f"{student.name} from {student.house}")
+    
+   
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student(name, house)
-    return student
+    
+   
+    return Student(name, house)
 
 
 
