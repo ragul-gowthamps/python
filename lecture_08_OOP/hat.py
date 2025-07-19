76336297b705b987e59a8416a277fe77c051960b
ragul-gowthamps <100247376+ragul-gowthamps@users.noreply.github.com>
2025-07-19 11:00:28 +0100

    @class method

--- lecture_08_OOP/hat.py
+++ lecture_08_OOP/hat.py
@@ -1,8 +1,25 @@
-class Hat:
-    def sort(self, name):
-        print(name, "is in" , ""somehousr")  
+import random
+# class Hat:
+#     def __init__(self):
+#         self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+        
+#     def sort(self, name):
+#         print(name, "is in" , random.choice(self.houses))  
     
 
 
-hat = Hat()
-hat.sort("Harry")
+# hat = Hat()
+# hat.sort("Harry")
+
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Hat:
+    
+    houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+        
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in" , random.choice(cls.houses))  
+        
+Hat.sort("Harry")
