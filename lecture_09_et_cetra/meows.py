f8551c559c8580200e9f1669074b287218bdfeab
ragul-gowthamps <100247376+ragul-gowthamps@users.noreply.github.com>
2025-07-20 19:43:55 +0100

    meows.py argparser

--- lecture_09_et_cetra/meows.py
+++ lecture_09_et_cetra/meows.py
@@ -1,10 +1,26 @@
-import sys
+# import sys
 
-if len(sys.argv) == 1:
+# if len(sys.argv) == 1:
+#     print("meow")
+# elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+#     n = int(sys.argv[2])
+#     for _ in range(n):
+#         print("meow")
+# else:
+#     print("usage: meows.py")
+
+
+
+
+
+
+
+#/////////////////////////////////////////////////////////
+
+import argparse
+parser = argparse.ArgumentParser(description="Print 'meow' a specified number of times.")
+parser.add_argument("-n", help="Number of times to print 'meow'", default = 1, type=int)
+args = parser.parse_args()
+
+for _ in range(int(args.n)):
     print("meow")
-elif len(sys.argv) == 3 and sys.argv[1] == "-n":
-    n = int(sys.argv[2])
-    for _ in range(n):
-        print("meow")
-else:
-    print("usage: meows.py")
